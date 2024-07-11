4f7fdad15175a8a26e9889f63f19d78dcd93ab26
Creat1ve <shubhdayal.shrivastava@gmail.com>
2024-07-11 23:12:50 +0530

    fixed

--- app/News/page.js
+++ app/News/page.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Image from 'next/image'
 
 const News = () => {
   return (
     <div className="bg-black min-h-screen text-white overflow-x-hidden">
       <Header />
       <div className="flex flex-row gap-10 mx-[20vw]">
-      <img className="h-[80vh]" src='/better.jpg'></img>
-      <img className="h-[80vh]" src='/better2.jpg'></img>
+      <Image className="h-[80vh]" src='/better.jpg' alt=""></Image>
+      <Image className="h-[80vh]" src='/better2.jpg' alt=""></Image>
       <Footer />
       </div>
     </div>
